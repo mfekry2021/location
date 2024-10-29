59c6a8eda6ec3f0164359f9ad036ef497e9915d3
Fekry <mahmoud.fekry@skopenow.com>
2024-10-29 13:34:33 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='feko-location',  # Name of the package
+    version='0.1',
+    packages=find_packages(),
+    description='A description of your package',
+    author='Your Name',
+    author_email='youremail@example.com',
+    url='https://github.com/mfekry2021/location',
+    install_requires=[
+        "requests",
+    ],
+)
